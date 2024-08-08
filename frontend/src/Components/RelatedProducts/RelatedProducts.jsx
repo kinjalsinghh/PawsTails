4912ff13551c36bd9ba5b9242e5a70a8571269a6
kinjalsinghh <118274996+kinjalsinghh@users.noreply.github.com>
2024-08-08 16:12:26 +0530

    Update RelatedProducts.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,17 +1,24 @@
 import Item from '../Items/Item'
 import './RelatedProducts.css'
-import data_product from '../Assets/data'
-
+import axios from 'axios'
+import { useState,useEffect } from 'react'
 function RelatedProduct(props){
+    const [related_products,setRelated_products]=useState([])
+    useEffect(()=>{
+        axios.get('https://pawstails-backend.onrender.com/bestsellers')
+        .then((res)=>{
+            setRelated_products(res.data)
+        })
+    },[])
     return(
         <div className='relatedproducts'>
             <hr style={{border:"none", background:"#efefef", height:"2px", width:"100%"}}/>
            <h1>Related products</h1>
            <hr/>
            <div className='relatedproducts-item'>
-             {data_product.map((item,i)=>{
+             {related_products.map((item,i)=>{
                 
-                    return <Item key={i} name={item.name} id={item.id} image={item.image} medium_price={item.medium_price} medium_old_price={item.medium_old_price}/>
+                    return <Item key={i} name={item.name} id={item.id} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
                    
              })}
            </div>
